5659f58e2d4ac2b24ce006e19cc2c8453620b8e7
Ernesto Serrano <info@ernesto.es>
2015-01-11 14:12:19 +0100

    date changes

--- evitacontroles/twitter.py
+++ evitacontroles/twitter.py
@@ -35,24 +35,24 @@ def get_twits_with_geo(palabra):
 	for r in tweepy.Cursor(api.user_timeline).items(200):
 		if palabra in r.text:
 			fecha = str(r.created_at)
-			if fecha <= time.strftime('%Y-%m-%d'):
-				if str(r.geo) != 'None':
-					texto = r.text
-					coordenadas = str(r.geo).split('[')[1].split(']')[0]
-					lat = coordenadas.split(',')[0]
-					lon = coordenadas.split(',')[1]
-					lista.append({
-  							"fields":{
-    							"category":None,
-     							"text":texto,
-     							"date":fecha,
-     							"user":"Bot de dai",
-     							"lat":lat,
-     							"lng":lon
-  							},
-  							"model":"evitacontroles.userpoint",
-  							"pk":1
-						},)
+			#if fecha <= time.strftime('%Y-%m-%d'):
+			if str(r.geo) != 'None':
+				texto = r.text
+				coordenadas = str(r.geo).split('[')[1].split(']')[0]
+				lat = coordenadas.split(',')[0]
+				lon = coordenadas.split(',')[1]
+				lista.append({
+							"fields":{
+							"category":None,
+ 							"text":texto,
+ 							"date":fecha,
+ 							"user":"Bot de dai",
+ 							"lat":lat,
+ 							"lng":lon
+							},
+							"model":"evitacontroles.userpoint",
+							"pk":1
+					},)
 	return json.dumps(lista)
 #print(get_twits_with_geo('#evitacontrol'))
 
